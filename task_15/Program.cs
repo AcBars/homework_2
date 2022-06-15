b0d3df62771a50f45fb5034ef9faa9fa333ab86b
Artem Mikhailob <mobail_2003@bk.ru>
2022-06-15 16:54:24 +0300

    task_15 ver.03 Не работает

--- task_15/Program.cs
+++ task_15/Program.cs
@@ -6,10 +6,52 @@
 
 using static System.Console;
 Clear();
-string [] wekend = {"Понедельник", "Вторник", "Среда", "Четвер", "Пятница", "Суббота", "Воскресенье"};
+string [] wekend = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
 WriteLine("Введите день недели");
-int number_day = Convert.ToInt32(ReadLine());
-if (number_day<1||number_day>=8) WriteLine("Вы ввели не день недели");
-else WriteLine(number_day>5 ? $"{wekend[number_day-1]}- это день выходной" : $"{wekend[number_day-1]}- этот день рабочий");
+//string number_day = ReadLine();
+string input = ReadLine();
+if (decimal.TryParse(input, out decimal number_day))
+{
+    int day = int.Parse(input);
+    WriteLine(day > 5 ? $"{wekend[day - 1]}- это выходной день" : $"{wekend[day - 1]}- это рабочий день");
+    WriteLine();
+}
+ 
+//else
+//WriteLine("Вам же сказали: ввести ЧИСЛО!");
+
+//WriteLine(number_day);
+
+
+
+// {
+//     number_day = int.TryParse(number_day);
+//     WriteLine(number_day > 5 ? $"{wekend[number_day - 1]}- это выходной день" : $"{wekend[number_day - 1]}- это рабочий день");
+//     WriteLine();
+// }
+if (number_day==wekend[0]) WriteLine("Понедельник - рабочий день недели.");
+else if (number_day==wekend[1]) WriteLine("Вторник - рабочий день недели.");
+else if (number_day==wekend[2]) WriteLine("Среда - рабочий день недели.");
+else if (number_day==wekend[3]) WriteLine("Четверг - рабочий день недели.");
+else if (number_day==wekend[4]) WriteLine("Пятница - рабочий день недели.");
+else if (number_day==wekend[5]) WriteLine("Суббота - выходной день недели.");
+else if (number_day==wekend[6]) WriteLine("Воскресенье - выходной день недели.");
+else WriteLine("Неправильный ввод.");
+
+
+//Clear();
+
+// if (number_day < 1 || number_day >= 8)
+// {
+//     WriteLine("Вы ввели не день недели");
+//     WriteLine();
+// }
+// else
+// {
+//     WriteLine(number_day > 5 ? $"{wekend[number_day - 1]}- это выходной день" : $"{wekend[number_day - 1]}- это рабочий день");
+//     WriteLine();
+// }
+
 
 // WriteLine(wekend[number_day-1]);
+
