028f1f4bdac217e361ea3d78f9acc7de9c11dd75
Artem Mikhailob <mobail_2003@bk.ru>
2022-06-15 14:33:11 +0300

    task_15 ver.02 Добавил вывод дня недели

--- task_15/Program.cs
+++ task_15/Program.cs
@@ -6,6 +6,10 @@
 
 using static System.Console;
 Clear();
+string [] wekend = {"Понедельник", "Вторник", "Среда", "Четвер", "Пятница", "Суббота", "Воскресенье"};
 WriteLine("Введите день недели");
 int number_day = Convert.ToInt32(ReadLine());
-WriteLine(number_day>5 ? "Это день выходной" : "Этот день рабочий");
+if (number_day<1||number_day>=8) WriteLine("Вы ввели не день недели");
+else WriteLine(number_day>5 ? $"{wekend[number_day-1]}- это день выходной" : $"{wekend[number_day-1]}- этот день рабочий");
+
+// WriteLine(wekend[number_day-1]);
